dcbd59f9052f15df158a4bfa7e151cd43c84e684
s.teleshev <s.teleshev@mu.se>
2024-01-25 19:50:43 +0100

    Revert "CB-4429 adds maxSize to resize state"
    This reverts commit 0894ef18c79a412f45bfd0af626f3adc1e8fc47a.

--- src/Split.tsx
+++ src/Split.tsx
@@ -140,10 +140,6 @@ export class Split extends React.Component<SplitProps, ISplitState> {
           return `${this.state.minSize}px`
         }
 
-        if (this.state.size >= this.state.maxSize) {
-          return `${this.state.maxSize}px`
-        }
-
         if (this.state.size === -1) {
           if (this.state.minSize > 0) {
             return `${this.state.minSize}px`
