5f2dd076444ef696e2a51034e02a6e3179b3c051
s.teleshev <s.teleshev@mu.se>
2024-01-25 19:53:01 +0100

    CB-4429 adds minSize/maxSize via css styles to use auto margin

--- src/Pane.tsx
+++ src/Pane.tsx
@@ -30,7 +30,29 @@ export function Pane(props: PaneProps) {
       if(patchedStyle.flexBasis === 'auto' && basis) {
         patchedStyle.flexBasis = basis;
       }
-    }                                                                                         
+    } 
+    
+    if(state.split == 'vertical') {
+      if(state.minSize > 0) {
+        patchedStyle.minWidth = state.minSize;
+      }
+      if(state.maxSize > 0) {
+        patchedStyle.maxWidth = state.maxSize;
+      }
+      if(state.maxSize < 0) {
+        patchedStyle.maxWidth = `calc(100% + ${state.maxSize}px)`;
+      }
+    } else {
+      if(state.minSize > 0) {
+        patchedStyle.minHeight = state.minSize;
+      }
+      if(state.maxSize > 0) {
+        patchedStyle.maxHeight = state.maxSize;
+      }
+      if(state.maxSize < 0) {
+        patchedStyle.maxHeight = `calc(100% + ${state.maxSize}px)`;
+      }
+    }
 
     Object.assign(ref.current.style, patchedStyle);
   })
